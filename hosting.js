d76e9453b792fd1af5db9f414bb6b3bcdd6d711b
Tushar <Tushar@gmail.com>
2023-08-19 12:06:29 +0530

    added

---
+++ hosting.js
@@ -0,0 +1,3 @@
+console.log(x);
+let x = 10;
+console.log(x);
---
+++ scope.js
@@ -0,0 +1,21 @@
+// function example()
+// {
+//     var x = 10;
+//     if(true)
+//     {
+//         var y = 20;
+//     }
+// console.log(x);
+// }
+
+// example();
+
+function example(){
+const x = 10;
+if(true){
+    const y = 20;
+}
+console.log(x)
+console.log(y)
+}
+ example();
